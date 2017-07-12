f2b445d5f838f521d188f1ac14041371e19462b9
cobe <cobe.lee@live.com>
2017-07-12 15:43:44 +0800

    回收公司报价

---
+++ LB.WebUI/UserControls/AjaxMessageBox.ascx
@@ -0,0 +1,35 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="AjaxMessageBox.ascx.cs"
+    Inherits="AjaxControl_AjaxMessageBox" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<style type="text/css">
+    .MessageDialog {
+        background-color: #CCCCFF;
+        border-width: 3px;
+        border-style: solid;
+        border-color: Gray;
+        padding: 10px;
+        width: 350px;
+        text-align: center;
+    }
+
+    .MessageDialogBackground {
+        background-color: #6699CC;
+        filter: alpha(opacity=70);
+        opacity: 0.7;
+    }
+</style>
+<asp:Button ID="tbTest" runat="server" Text=""  Width="1px" Height="1px" />
+<asp:Panel ID="pnlMessage" runat="server" Class="MessageDialog">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional" OnLoad="UpdatePanel1_Load">
+        <ContentTemplate>
+            <asp:Label ID="lbMessage" runat="server" Text=""></asp:Label><br />
+            <div style="padding: 10px 0px 10px 0px; text-align: center;">
+                <asp:Button ID="btnClose" runat="server" Text="确定" OnClick="btnClose_Click" CausesValidation="false" />
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Panel>
+<asp:ModalPopupExtender ID="ModalPopupExtender1" runat="server" TargetControlID="tbTest"
+    PopupControlID="pnlMessage" BackgroundCssClass="MessageDialogBackground">
+</asp:ModalPopupExtender>
---
+++ LB.WebUI/UserControls/AjaxMessageBox.ascx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AjaxControl_AjaxMessageBox : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    #region 属性
+    /// <summary>
+    /// 对话框的宽度
+    /// </summary>
+    public Unit Width
+    {
+        get { return pnlMessage.Width; }
+        set { pnlMessage.Width = value; }
+    }
+
+    /// <summary>
+    /// 对话框的高度
+    /// </summary>
+    public Unit Height
+    {
+        get { return pnlMessage.Width; }
+        set { pnlMessage.Height = value; }
+    }
+
+    public string MessageText
+    {
+        get { return lbMessage.Text; }
+        set { lbMessage.Text = value; }
+    }
+
+    public string TargetControlID
+    {
+        get { return ModalPopupExtender1.TargetControlID; }
+        set { ModalPopupExtender1.TargetControlID = value; }
+    }
+    #endregion
+
+    public void Show()
+    {
+        ModalPopupExtender1.Show();
+    }
+
+    public void Show(string messageText)
+    {
+        lbMessage.Text = messageText;
+        ModalPopupExtender1.Show();
+    }
+
+    public void Hide()
+    {
+        ModalPopupExtender1.Hide();
+    }
+
+    protected void btnClose_Click(object sender, EventArgs e)
+    {
+        ModalPopupExtender1.Hide();
+    }
+
+    protected void UpdatePanel1_Load(object sender, EventArgs e)
+    {
+        UpdatePanel1.Update();
+    }
+}
