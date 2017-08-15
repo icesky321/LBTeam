844d9b0f50300837154ca4efd1aa74f8350a78de
cao <icesky321>
2017-08-15 14:13:27 +0800

    添加服务号端今日电瓶资讯

---
+++ WxQYUI/MP/TodayNews.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MP_TodayNews : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            hfBeginDate.Value = System.DateTime.Now.AddDays(-1).ToString();
+            hfEndDate.Value = System.DateTime.Now.AddDays(1).ToString();
+        }
+    }
+}
