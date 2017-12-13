407b262b36ccea453d615eb8433621a6f1f148f6
cao <icesky321>
2017-12-13 09:45:43 +0800

    重获密码修复

--- WxQYUI/Login/RetrievalPwd.aspx.cs
+++ WxQYUI/Login/RetrievalPwd.aspx.cs
@@ -20,7 +20,7 @@ public partial class Login_RetrievalPwd : System.Web.UI.Page
         string pwd = user.GetPassword();
         LB.BLL.SMS sms = new LB.BLL.SMS();
         //sms.SendSMS("13958202267", "[绿宝科技]验证码：123456，此验证码即为您的当前登录密码，请登录后立即更改您的登录密码。");
-        sms.SendSMS("13958202267", "验证码：" + pwd + "。此验证码即为您的账户登录密码，请牢记或者重设密码。【绿宝】");
+        sms.SendSMS(tbMobileNum.Text, "验证码：" + pwd + "。此验证码即为您的账户登录密码，请牢记或者重设密码。【绿宝】");
         Response.Redirect("~/ErrorPage/Success.aspx");
     }
 }
